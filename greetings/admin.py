84091601a3f8429c1404cbfef20002f2836ca4ac
Zhao Li <zhaol@hawaii.edu>
2021-07-01 00:28:36 -1000

    make greetings relate to farewells using a through table

--- greetings/admin.py
+++ greetings/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Greeting
+from .models import Greeting, Farewell, SalutationValedictionPair
 
 # Register your models here.
-admin.site.register(Greeting)
+admin.site.register([
+  Greeting,
+  Farewell,
+  SalutationValedictionPair,
+])
--- greetings/models.py
+++ greetings/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Farewell(models.Model):
+    message = models.CharField(max_length=200)
+
 class Greeting(models.Model):
     message = models.CharField(max_length=200)
+    farewells = models.ManyToManyField(Farewell, through="SalutationValedictionPair")
+
+class SalutationValedictionPair(models.Model):
+    greeting = models.ForeignKey(Greeting, on_delete=models.CASCADE)
+    farewell = models.ForeignKey(Farewell, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
 
